2d018596f4be74b9a3d2026f27c11979256f40e0
devendrajadon93 <devendersingh1993apps@gmail.com>
2021-05-12 13:02:57 +0530

    List Employee API

--- src/main/java/com/employee/controller/EmployeeController.java
+++ src/main/java/com/employee/controller/EmployeeController.java
@@ -10,10 +10,15 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.employee.entity.Employee;
 import com.employee.service.EmployeeService;
+import com.employee.utility.CommonConstants;
+import com.employee.view.model.ListModal;
+import com.employee.view.model.ViewEmployee;
 
 @RestController
 public class EmployeeController {
@@ -38,5 +43,18 @@ public class EmployeeController {
 		int status = employeeService.deleteEmployeeById(empId);
 		LOGGER.debug("Deleting a role entry with information {}",status);
 	}
+	
+	@GetMapping("/employee")
+	public  @ResponseBody ListModal<ViewEmployee> employeeList(@RequestParam(name = "searchTerm",required = false,defaultValue = CommonConstants.EMPTY_STRING) String searchTerm,
+			 @RequestParam(name = "pageNo",required = false,defaultValue = CommonConstants.ZERO) int pageNo,
+			 @RequestParam(name = "pageSize",required = false,defaultValue = CommonConstants.TEN) int pageSize){
+		
+		 LOGGER.debug("Start Geting roles list with information");
+		 ListModal<ViewEmployee> roleList = employeeService.getEmployeeList(searchTerm,pageNo,pageSize);
+		 LOGGER.debug("End Geting roles list with information {} " , roleList);
+		 return roleList;
+	
+	}
+	
 	
 }
